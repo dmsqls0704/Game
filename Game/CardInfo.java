369786ee660cd77df4faa0a45e2e742abb92b4d0
dmsqls0704 <114456407+dmsqls0704@users.noreply.github.com>
2023-11-21 21:08:46 +0900

    Add files via upload

---
+++ Game/CardInfo.java
@@ -0,0 +1,182 @@
+package Game;
+
+import javax.swing.*;
+import javax.swing.plaf.ColorUIResource;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+/**
+ * 
+ * 정보 열람 화면을 실행하는 클래스이다. 
+ * 사용자 닉네임, 최고 점수, 이미지를 보여준다.
+ * 
+ */
+public class CardInfo extends JPanel{
+	private CardLayout cardLayout;
+	private JPanel cardPanel;
+	private String[] data;
+	/**
+	 * CardInfo 클래스의 생성자이다.
+	 * @param layout	CardLayout을 설정하는데 사용되는 객체
+	 * @param panel		CardLayout이 포함된 패널
+	 * @param mainPage	메인 화면으로 돌아가기 위한 객체
+	 * @param data		사용자 정보를 담은 배열 (닉네임, 최고 점수)
+	 */
+	public CardInfo(CardLayout layout, JPanel panel, MainPage mainPage, String[] data){
+		cardLayout = layout;
+        cardPanel = panel;
+        this.data = data;
+        
+		setLayout(cardLayout);
+		GridBagConstraints gbc = new GridBagConstraints();
+				
+		JPanel panelI = new JPanel();
+        panelI.setLayout(new GridBagLayout());
+        panelI.setBackground(new Color(248,244,235));
+        
+        JPanel userRank = new JPanel();
+        userRank.setLayout(new GridBagLayout());
+        userRank.setBackground(new Color(237, 227, 206));
+        
+        gbc.gridx=0;
+        gbc.gridy=0;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.gridwidth = 4; 
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.weightx = 1.0;
+        panelI.add(userRank,gbc);
+        
+        gbc.gridwidth =1;
+        gbc.fill = GridBagConstraints.NONE;
+//        Font font = new Font("Yeongdeok sea", Font.PLAIN, 30);
+        Font font = Utility.yeongdeok_sea(50);
+        
+        ImageIcon rankI = new ImageIcon(getClass().getResource("/image/trophy.png"));
+        Image originalImage = rankI.getImage();
+        Image scaledImage = originalImage.getScaledInstance(85,90,Image.SCALE_SMOOTH);
+        rankI = new ImageIcon(scaledImage);
+        JLabel rankImage = new JLabel(rankI);
+        gbc.insets = new Insets(10,10,10,10);
+        userRank.add(rankImage);
+        
+        gbc.gridwidth = 2;
+        JLabel userNick = new JLabel(data[0]);
+        JLabel userScore = new JLabel(data[1]);
+        userNick.setMaximumSize(new Dimension(200, 80)); 
+//        userNick.setPreferredSize(new Dimension(200, 80)); 
+        
+        userNick.setFont(font);
+        userScore.setFont(font);
+        userScore.setForeground(new Color(125, 159, 104));
+        
+        gbc.gridx=1;
+        gbc.insets = new Insets(10,10,10,10);
+        userRank.add(userNick,gbc);
+        
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(10,340,10,10);
+        userRank.add(userScore,gbc);
+        
+        JButton backButton = new RoundedButton("");
+        ImageIcon homeImage = new ImageIcon(getClass().getResource("/image/yongyonghome.png"));
+        Image originalhomeImage = homeImage.getImage();
+        Image scaledhomeImage = originalhomeImage.getScaledInstance(100,100, Image.SCALE_SMOOTH);
+        homeImage= new ImageIcon(scaledhomeImage);
+        
+        backButton.setIcon(homeImage);
+        backButton.setPreferredSize(new Dimension(80,80));
+        backButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                cardLayout.show(cardPanel, "mainPanel"); 
+            }
+        });
+        backButton.setBackground(new Color(125, 159, 104));
+        backButton.setBorder(BorderFactory.createLineBorder(new Color(80, 102, 67)));
+        UIManager.put("Button.focus", new ColorUIResource(new Color(125, 159, 104)));
+
+        gbc.gridwidth = 1;
+        gbc.gridx=3;
+        gbc.anchor = GridBagConstraints.EAST;
+        gbc.insets = new Insets(10,50,10,10);
+        userRank.add(backButton,gbc);
+       
+        JScrollPane scroll = new JScrollPane();
+        JPanel imagePanel = new JPanel();
+        imagePanel.setLayout(new GridBagLayout());
+        imagePanel.setBackground(new Color(248,244,235));
+        
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+
+        
+        gbc.anchor = GridBagConstraints.WEST;
+        
+        addImage(imagePanel, "/image/bongji.jpg",0,1,gbc);
+        addImage(imagePanel, "/image/clockt.jpg",1,1,gbc);
+        addImage(imagePanel, "/image/baekdo.jpg",2,1,gbc);
+        addImage(imagePanel, "/image/dido.jpg",0,2,gbc);
+        addImage(imagePanel, "/image/hongdo.jpg",1,2,gbc);
+        addImage(imagePanel, "/image/playground.jpg",2,2,gbc);
+        addImage(imagePanel, "/image/yongbongt.jpg",0,3,gbc);
+        addImage(imagePanel, "/image/yongji.jpg",1,3,gbc);
+        
+        addImage(imagePanel, "/image/club1.jpg",0,4,gbc);
+        addImage(imagePanel, "/image/club2.jpg",1,4,gbc);
+        addImage(imagePanel, "/image/club3.jpg",2,4,gbc);
+        addImage(imagePanel, "/image/club4.jpg",0,5,gbc);
+        addImage(imagePanel, "/image/club5.jpg",1,5,gbc);
+        addImage(imagePanel, "/image/club6.jpg",2,5,gbc);
+        addImage(imagePanel, "/image/club7.jpg",0,6,gbc);
+        addImage(imagePanel, "/image/club8.jpg",1,6,gbc);
+        addImage(imagePanel, "/image/club9.jpg",2,6,gbc);
+        addImage(imagePanel, "/image/club10.jpg",0,7,gbc);
+        addImage(imagePanel, "/image/club11.jpg",1,7,gbc);
+        addImage(imagePanel, "/image/club12.jpg",2,7,gbc);
+        addImage(imagePanel, "/image/club13.jpg",0,8,gbc);
+        addImage(imagePanel, "/image/club14.jpg",1,8,gbc);
+        addImage(imagePanel, "/image/club15.jpg",2,8,gbc);
+        addImage(imagePanel, "/image/club16.jpg",0,9,gbc);
+        addImage(imagePanel, "/image/club17.jpg",1,9,gbc);
+        addImage(imagePanel, "/image/club18.jpg",2,9,gbc);
+        addImage(imagePanel, "/image/club19.jpg",0,10,gbc);
+        addImage(imagePanel, "/image/club20.jpg",1,10,gbc);
+        addImage(imagePanel, "/image/club21.jpg",2,10,gbc);
+        addImage(imagePanel, "/image/club22.jpg",0,10,gbc);
+        addImage(imagePanel, "/image/club23.jpg",1,10,gbc);
+        addImage(imagePanel, "/image/club24.jpg",2,10,gbc);
+        
+        
+        scroll.setViewportView(imagePanel);
+        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scroll.getVerticalScrollBar().setBackground(new Color(248, 244, 235));
+        scroll.getHorizontalScrollBar().setBackground(new Color(248, 244, 235));
+
+       
+        panelI.add(scroll,gbc);
+//       add(scroll, "InfoPanel");
+        add(panelI, "InfoPanel");
+	}
+	
+	 private void addImage(JPanel panel, String imagePath, int gridX, int gridY, GridBagConstraints gbc) {
+		ImageIcon imageIcon = new ImageIcon(getClass().getResource(imagePath));
+		Image originalImage = imageIcon.getImage();
+		Image scaledImage = originalImage.getScaledInstance(225,300, Image.SCALE_SMOOTH);
+		imageIcon = new ImageIcon(scaledImage);
+		
+		JLabel imageLabel = new JLabel(imageIcon);
+		gbc.weightx = 1.0; 
+	    gbc.weighty = 1.0;
+		gbc.gridx = gridX;
+        gbc.gridy = gridY;
+        gbc.insets = new Insets(10,10, 10, 10);
+        panel.add(imageLabel, gbc);
+	}
+	 
+	
+}
---
+++ Game/JoinScreen.java
@@ -0,0 +1,161 @@
+package Game;
+
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.plaf.ColorUIResource;
+
+/** 
+ * 
+ * 회원가입 화면을 실행하는 클래스이다. 
+ * 
+ */
+public class JoinScreen extends JPanel{
+	private CardLayout cardLayout;
+	private JPanel cardPanel;
+	private JTextField nickname;
+	private JTextField password;
+	private DataManager dataManager;
+	/**
+	 * JoinScreen 클래스의 생성자이다.
+	 * @param layout	CardLayout을 설정하는데 사용되는 객체
+	 * @param panel 	CardLayout이 포함된 패널
+	 */
+	public JoinScreen(CardLayout layout, JPanel panel) {
+		cardLayout = layout;
+        cardPanel = panel;
+        dataManager = new DataManager();
+        
+        setLayout(cardLayout);
+        
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx=0;
+        gbc.gridy=0;
+        
+        JPanel panelJ = new JPanel();
+        panelJ.setLayout(new GridBagLayout());
+        panelJ.setBackground(new Color(248,244,235));
+        
+        Font titleFont = Utility.yeongdeok_haeparang(50);
+        Font font = Utility.yeongdeok_sea(40);
+        
+        /** 회원가입 화면임을 알려주는 제목을 생성한다. */
+        JLabel titleJoin = new JLabel("회원가입");
+        titleJoin.setFont(titleFont);
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(10,10,10,100);
+        panelJ.add(titleJoin, gbc);
+        
+        /** 닉네임을 입력 받는 필드임을 알려주는 제목을 생성한다. */
+        JLabel titleName = new JLabel("닉네임");
+        gbc.gridy=1;
+        titleName.setFont(font);
+        gbc.insets = new Insets(10,10,10,100);
+        panelJ.add(titleName, gbc);
+        /** 닉네임을 입력 받을 수 있는 필드를 생성한다. */
+        nickname = new JTextField(10);
+        gbc.gridy=2;
+        nickname.setFont(font);
+        gbc.insets = new Insets(10,10,10,100);
+        panelJ.add(nickname,gbc);
+        
+        /** 비밀번호를 입력 받는 필드임을 알려주는 제목을 생성한다. */
+        JLabel titlePW = new JLabel("비밀번호");
+        gbc.gridy=3;
+        titlePW.setFont(font);
+        gbc.insets = new Insets(10,10,10,100);
+        panelJ.add(titlePW, gbc);
+        /** 비밀번호를 입력 받을 수 있는 필드를 생성한다. */
+        password = new JPasswordField(10);
+        gbc.gridy=4;
+        password.setFont(font);
+        gbc.insets = new Insets(10,10,10,100);
+        panelJ.add(password,gbc);
+       
+        Font fontB = Utility.yeongdeok_sea(30);
+       
+        JButton backButton = new RoundedButton("");
+        backButton.setPreferredSize(new Dimension(80, 80));
+        backButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		cardLayout.show(cardPanel,"startPanel");
+        	}
+        });
+        backButton.setBackground(new Color(125, 159, 104));
+        backButton.setBorder(BorderFactory.createLineBorder(new Color(80, 102, 67)));
+    	UIManager.put("Button.focus", new ColorUIResource(new Color(125, 159, 104)));
+    	
+    	ImageIcon homeImage = new ImageIcon(getClass().getResource("/image/yongyonghome.png"));
+        Image originalhomeImage = homeImage.getImage();
+        Image scaledhomeImage = originalhomeImage.getScaledInstance(100,100, Image.SCALE_SMOOTH);
+        homeImage= new ImageIcon(scaledhomeImage);
+        
+        backButton.setIcon(homeImage);
+        
+    	gbc.anchor = GridBagConstraints.EAST;
+    	gbc.gridx=1;
+    	gbc.gridy=0;
+    	gbc.insets = new Insets(10,10,10,10);
+    	panelJ.add(backButton, gbc);
+    	
+        /** 확인 버튼을 생성하고, 입력한 닉네임과 비밀번호를 저장한다. */
+        JButton checkButton = new RoundedButton("확인");
+        checkButton.setPreferredSize(new Dimension(150, 50));
+        checkButton.setFont(fontB);
+        checkButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+                if (nickname.getText().trim().isEmpty() || password.getText().trim().isEmpty()) {
+//                    JOptionPane.showMessageDialog(JoinScreen.this, "닉네임과 비밀번호를 입력하세요.", "입력 오류", JOptionPane.ERROR_MESSAGE);
+                	ErrorMessage.showErrorDialog("입력 오류", "닉네임과 비밀번호를 입력하세용.");
+                } 
+                else {
+                	if(dataManager.dataCheck(nickname.getText(),password.getText())) {
+//                		JOptionPane.showMessageDialog(JoinScreen.this, "이미 가입되었습니다.", "입력 오류", JOptionPane.ERROR_MESSAGE);
+                		ErrorMessage.showErrorDialog("입력 오류", "이미 가입되었어용.");
+                		cardLayout.show(cardPanel, "startPanel");
+                	}
+                	else if(!dataManager.pwCheck(password.getText())){
+//                		JOptionPane.showMessageDialog(JoinScreen.this, "비밀번호는 4자리로 입력하세요.", "입력 오류", JOptionPane.ERROR_MESSAGE);
+                		ErrorMessage.showErrorDialog("입력 오류", "비밀번호는 4자리로 입력하세용.");
+                		password.setText("");
+                	}
+                	else {
+                        dataManager.saveData(nickname.getText(), password.getText());
+                        dataManager.clear(nickname,password);
+                        cardLayout.show(cardPanel, "startPanel");
+                	}
+                }  
+        	}	
+        });
+        checkButton.setBackground(new Color(125, 159, 104));
+        checkButton.setBorder(BorderFactory.createLineBorder(new Color(80, 102, 67)));
+		UIManager.put("Button.focus", new ColorUIResource(new Color(125, 159, 104)));
+		
+		gbc.anchor = GridBagConstraints.EAST;
+		gbc.gridx=1;
+		gbc.gridy=4;
+		gbc.insets = new Insets(10,10,10,10);
+		panelJ.add(checkButton, gbc);
+    
+        add(panelJ, "joinPanel");
+	}
+	
+}
